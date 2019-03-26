a5f588cf71c33b78d7675fe13c670e67f9dbd8ec
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-26 09:30:24 -0600

    Created login and register methods

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  url = 'https://reqres.in/api/login';
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json'});
+  url = 'https://reqres.in/';
   constructor(private http: HttpClient) { }
 
   login(credentials) {
-    return this.http.post(this.url, JSON.stringify(credentials))
+    return this.http.post(this.url + 'api/login', JSON.stringify(credentials), {headers: this.headers})
     .subscribe(response => {
       console.log(response);
     }, error => {
       alert('something bad happen');
     });
+  }
+
+  register(credentials) {
+    console.log(credentials);
+    return this.http.post(this.url + 'api/register', JSON.stringify(credentials), {headers: this.headers})
+      .subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+        alert('The register can not be completed');
+      });
+  }
+
+  getAllUsers() {
+    return this.http.get(this.url + 'api/users');
   }
 }
