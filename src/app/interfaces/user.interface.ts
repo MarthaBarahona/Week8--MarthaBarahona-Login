7994e03d85891d48aecc196f046bf108dfddcdca
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-26 14:58:20 -0600

    Added image property to form

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -1,9 +1,8 @@
 export interface User {
-    firstname: string;
-    lastname: string;
-    username: string;
-    email: string;
-    avatar: string;
-    password: string;
-
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  avatar: string;
+  password: string;
 }
