ce092aa3ae990c718e728a711f342348b4d8f8d2
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-28 17:02:01 -0600

    Erased some spaces

--- src/app/home/components/user-profile/user-profile.component.ts
+++ src/app/home/components/user-profile/user-profile.component.ts
@@ -1,15 +1,18 @@
+import { User } from './../../../interfaces/user.interface';
+import { UserService } from './../../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.sass']
+  styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor(private actualUser: UserService) { }
 
   ngOnInit() {
+    this.user = this.actualUser.getUser();
   }
 
 }
